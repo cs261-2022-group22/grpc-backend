31c0fc6ac9f5e5d83e18b1aef66c7e2d76fe127d
Varun Chodanker <57418924+varunchodanker@users.noreply.github.com>
2022-02-16 13:27:35 +0000

    Implemented the connection pool strategy with the gRPC servicers  to provide as many connections and cursors as the number of threads that are used. Then the servicers extract a pair of these from the pool so that they can interact with the database and perform their desired actions concurrently.

--- authentication_server.py
+++ authentication_server.py
@@ -1,5 +1,8 @@
 from concurrent import futures
 import logging
+import time
+from threading import Lock
+from queue import Queue
 
 import grpc
 import authentication_pb2
@@ -12,24 +15,20 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+mutex = Lock() #to prevent race conditions
+
+connCurList = [] #does not get manipulated - another version of the collection below
+connCurQueue = Queue(maxsize=10) #connections to database and corresponding cursors
+
 class Authenticate(authentication_pb2_grpc.AuthenticateServicer):
     def TryLogin(self, request, context):
+        mutex.acquire()
+        (conn, cur) = connCurQueue.get_nowait() #cursor for performing sql statements
+        mutex.release()
+
         response = authentication_pb2.AuthenticateReply()
         response.status = "FAILURE" #failure biased
 
-        conn = psycopg2.connect( #connection to database
-            "dbname=" + 
-            os.getenv("POSTGRES_DATABASE") + 
-            " user=" + 
-            os.getenv("POSTGRES_USER") + 
-            " password=" + 
-            os.getenv("POSTGRES_PASSWORD") + 
-            " host=" + 
-            os.getenv("POSTGRES_HOST") + 
-            " port=" + 
-            os.getenv("POSTGRES_PORT")
-        )
-        cur = conn.cursor() #cursor for performing sql statements
         
         ###
         cur.execute("SELECT passwordHash, accountId FROM Account WHERE email=%s;", (request.username,))
@@ -45,24 +44,50 @@ class Authenticate(authentication_pb2_grpc.AuthenticateServicer):
 
         conn.commit()
 
-        cur.close()
-        conn.close()
+        mutex.acquire()
+        connCurQueue.put_nowait((conn, cur))
+        mutex.release()
 
         return response
 
-        # if request.username == "test@gmail.com" and request.password == "test":
-        #     response.id = 1
-        #     response.status = "SUCCESS"
-        # else:
-        #     response.status = "FAILURE" 
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     authentication_pb2_grpc.add_AuthenticateServicer_to_server(Authenticate(), server)
     print("Server started. Listening on port 50051.")
     server.add_insecure_port('[::]:50051')
+
+    #create a connection and corresponding cursor for each thread
+    for i in range(10):
+        conn = psycopg2.connect( #connection to database
+            "dbname=" + 
+            os.getenv("POSTGRES_DATABASE") + 
+            " user=" + 
+            os.getenv("POSTGRES_USER") + 
+            " password=" + 
+            os.getenv("POSTGRES_PASSWORD") + 
+            " host=" + 
+            os.getenv("POSTGRES_HOST") + 
+            " port=" + 
+            os.getenv("POSTGRES_PORT")
+        )
+        cur = conn.cursor()
+        connCurQueue.put_nowait((conn, cur))
+        connCurList.append((conn, cur))
+
     server.start()
-    server.wait_for_termination()
+    try:
+        while True:
+            time.sleep(100000)
+    except KeyboardInterrupt:
+            server.stop(10)
+
+    print("Server stopped.")
+    #clean up
+    for i in range(10):
+        (conn, cur) = connCurList[i]
+        cur.close()
+        conn.close()
 
 
 if __name__ == '__main__':
