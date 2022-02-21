fc7dfbfcabf4fc32a54ad5c8f396cbc9f714cfd2
Varun Chodanker <57418924+varunchodanker@users.noreply.github.com>
2022-02-21 22:13:03 +0000

    Updated common to add the new account profiles rpc. Implemented this on the server and produced example client code. Sample data added for mentor and mentee profiles.

--- put-data.sql
+++ put-data.sql
@@ -1,5 +1,7 @@
 INSERT INTO BusinessSector VALUES(1, 'Private Bank');
-INSERT INTO Account(name, email, passwordHash, dob, businessSectorId) 
-VALUES('John Doe', 'test@gmail.com', 
+INSERT INTO Account 
+VALUES(1, 'John Doe', 'test@gmail.com', 
 E'\\x243262243132245159367569413552717a613166754f4b533478524c4f5041724a33645270774970376f334e703671324f44524c4859424256755632', 
 DATE '1999-01-01', 1);
+INSERT INTO Mentor VALUES(1,1);
+INSERT INTO Mentee VALUES(1,1);
