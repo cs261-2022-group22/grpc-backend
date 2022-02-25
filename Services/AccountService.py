7d1bc61553abbda7ec36a660f7b0034587e6ac20
Moody <mooodyhunter@outlook.com>
2022-02-25 23:35:56 +0000

    migrate to betterproto

---
+++ Services/AccountService.py
@@ -0,0 +1,140 @@
+import os
+from datetime import datetime
+from queue import Queue
+from threading import Lock
+
+import bcrypt
+import protos.account_package as Account
+import psycopg2
+from grpclib.server import Server
+
+gRPCServer: Server
+
+connMutex = Lock()  # to prevent race conditions
+connCurList = []  # does not get manipulated - another version of the collection below
+connCurQueue: Queue = Queue(maxsize=10)  # connections to database and corresponding cursors
+
+
+class AccountService(Account.AccountServiceBase):
+    async def try_login(self, username: str, password: str) -> Account.AuthenticateReply:
+        connMutex.acquire()
+        (conn, cur) = connCurQueue.get_nowait()  # cursor for performing sql statements
+        connMutex.release()
+
+        response = Account.AuthenticateReply()
+        response.status = False  # failure biased
+
+        ###
+        cur.execute("SELECT passwordHash, accountId FROM Account WHERE email=%s;", (username,))
+        if (resultRow := cur.fetchone()) is not None:
+            # The default output format of bytes in the database is memory view. Thus, this
+            # must be converted to the bytes datatype for use with brcrypt functions.
+            storedPasswordHashBytes = (resultRow[0]).tobytes()
+            givenPasswordPlainBytes = password.encode("utf-8")
+            if bcrypt.checkpw(givenPasswordPlainBytes, storedPasswordHashBytes):
+                response.id = resultRow[1]
+                response.status = True
+        ###
+
+        conn.commit()
+
+        connMutex.acquire()
+        connCurQueue.put_nowait((conn, cur))
+        connMutex.release()
+
+        return response
+
+    async def register_user(self, name: str, date_of_birth: datetime, email: str, password: str, business_area_id: int) -> Account.RegistrationReply:
+        connMutex.acquire()
+        (conn, cur) = connCurQueue.get_nowait()  # cursor for performing sql statements
+        connMutex.release()
+
+        response = Account.RegistrationReply()
+        response.status = False  # failure biased
+
+        ###
+        # print(request.name)
+        # print(request.email)
+        # print(request.password)
+        # print(request.businessarea.id)
+        # print(request.businessarea.name)
+        print(date_of_birth)
+
+        ###
+
+        conn.commit()
+
+        connMutex.acquire()
+        connCurQueue.put_nowait((conn, cur))
+        connMutex.release()
+
+        return response
+
+    async def account_profiles(self, userid: int) -> Account.ProfilesReply:
+        connMutex.acquire()
+        (conn, cur) = connCurQueue.get_nowait()  # cursor for performing sql statements
+        connMutex.release()
+
+        response = Account.ProfilesReply()
+        response.is_mentor = False  # failure biased
+        response.is_mentee = False
+
+        ###
+        cur.execute("SELECT mentorId FROM Mentor WHERE accountId=%s;",
+                    (userid,))
+        if cur.fetchone() is not None:
+            response.is_mentor = True
+
+        cur.execute("SELECT menteeId FROM Mentee WHERE accountId=%s;",
+                    (userid,))
+        if cur.fetchone() is not None:
+            response.is_mentee = True
+        ###
+
+        conn.commit()
+
+        connMutex.acquire()
+        connCurQueue.put_nowait((conn, cur))
+        connMutex.release()
+
+        return response
+
+
+async def serve(port: int):
+    # create a connection and corresponding cursor for each thread
+    for _ in range(10):
+        # connection to database
+        conn: psycopg2.connection = psycopg2.connect(
+            "dbname=" +
+            os.getenv("POSTGRES_DATABASE", "mentoring") +
+            " user=" +
+            os.getenv("POSTGRES_USER", "") +
+            " password=" +
+            os.getenv("POSTGRES_PASSWORD", "") +
+            " host=" +
+            os.getenv("POSTGRES_HOST", "localhost") +
+            " port=" +
+            os.getenv("POSTGRES_PORT", "")
+        )
+        cur: psycopg2.cursor = conn.cursor()
+        connCurQueue.put_nowait((conn, cur))
+        connCurList.append((conn, cur))
+
+    global gRPCServer
+    gRPCServer = Server([AccountService()])
+    await gRPCServer.start("127.0.0.1", port)
+    print("Server started. Listening on port:", port)
+    await gRPCServer.wait_closed()
+
+
+def close():
+    if gRPCServer.is_serving:
+        gRPCServer.close()
+
+    # clean up
+    for i in range(10):
+        (conn, cur) = connCurList[i]
+        cur.close()
+        conn.close()
+    print("Server stopped.")
+    pass
--- account_server.py
+++ account_server.py
@@ -1,154 +1,25 @@
-from concurrent import futures
+import asyncio
 import logging
-import time
-from threading import Lock
-from queue import Queue
-
-import grpc
-import account_pb2
-import account_pb2_grpc
-
-import psycopg2
-import bcrypt
 import os
+
+import Services.AccountService as AccountService
 from dotenv import load_dotenv
 
-load_dotenv()
+if __name__ == '__main__':
+    load_dotenv()
+    logging.basicConfig()
 
-mutex = Lock() #to prevent race conditions
-
-connCurList = [] #does not get manipulated - another version of the collection below
-connCurQueue = Queue(maxsize=10) #connections to database and corresponding cursors
-
-class AccountServiceHandler(account_pb2_grpc.AccountServiceServicer):
-    def TryLogin(self, request, context):
-        mutex.acquire()
-        (conn, cur) = connCurQueue.get_nowait() #cursor for performing sql statements
-        mutex.release()
-
-        response = account_pb2.AuthenticateReply()
-        response.status = False #failure biased
-
-        
-        ###
-        cur.execute("SELECT passwordHash, accountId FROM Account WHERE email=%s;", (request.username,))
-        if (resultRow := cur.fetchone()) is not None:
-            # The default output format of bytes in the database is memory view. Thus, this 
-            # must be converted to the bytes datatype for use with brcrypt functions.
-            storedPasswordHashBytes = (resultRow[0]).tobytes()
-            givenPasswordPlainBytes = request.password.encode("utf-8")
-            if bcrypt.checkpw(givenPasswordPlainBytes, storedPasswordHashBytes):
-                response.id = resultRow[1]
-                response.status = True
-        ###
-
-        conn.commit()
-
-        mutex.acquire()
-        connCurQueue.put_nowait((conn, cur))
-        mutex.release()
-
-        return response
-
-    def UserRegistration(self, request, context):
-        mutex.acquire()
-        (conn, cur) = connCurQueue.get_nowait() #cursor for performing sql statements
-        mutex.release()
-
-        response = account_pb2.RegistrationReply()
-        response.status = False #failure biased
-
-        ###
-        # print(request.name)
-        # print(request.email)
-        # print(request.password)
-        # print(request.businessarea.id)
-        # print(request.businessarea.name)
-        # print(request.dateofbirth.ToDatetime())
-
-        ###
-
-        conn.commit()
-
-        mutex.acquire()
-        connCurQueue.put_nowait((conn, cur))
-        mutex.release()
-
-        return response
-
-    def AccountProfiles(self, request, context):
-        mutex.acquire()
-        (conn, cur) = connCurQueue.get_nowait() #cursor for performing sql statements
-        mutex.release()
-
-        response = account_pb2.ProfilesReply()
-        response.isMentor = False #failure biased
-        response.isMentee = False
-
-        ###
-        cur.execute("SELECT mentorId FROM Mentor WHERE accountId=%s;", 
-        (request.userid,))
-        if cur.fetchone() is not None:
-            response.isMentor = True
-        
-        cur.execute("SELECT menteeId FROM Mentee WHERE accountId=%s;", 
-        (request.userid,))
-        if cur.fetchone() is not None:
-            response.isMentee = True
-        ###
-
-        conn.commit()
-
-        mutex.acquire()
-        connCurQueue.put_nowait((conn, cur))
-        mutex.release()
-
-        return response        
-
-
-def serve():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    account_pb2_grpc.add_AccountServiceServicer_to_server(AccountServiceHandler(), server)
     port = int(os.getenv("GRPC_BACKEND_PORT") or 50051)
     if port < 1 or port > 65535:
         print("Invalid port number:", port)
         exit()
 
-    print("Server started. Listening on port:", port)
-    server.add_insecure_port('[::]:' + str(port))
-    #create a connection and corresponding cursor for each thread
-    for i in range(10):
-        conn = psycopg2.connect( #connection to database
-            "dbname=" + 
-            os.getenv("POSTGRES_DATABASE") + 
-            " user=" + 
-            os.getenv("POSTGRES_USER") + 
-            " password=" + 
-            os.getenv("POSTGRES_PASSWORD") + 
-            " host=" + 
-            os.getenv("POSTGRES_HOST") + 
-            " port=" + 
-            os.getenv("POSTGRES_PORT")
-        )
-        cur = conn.cursor()
-        connCurQueue.put_nowait((conn, cur))
-        connCurList.append((conn, cur))
+    async def main():
+        await AccountService.serve(port)
 
-    server.start()
     try:
-        while True:
-            time.sleep(100000)
+        loop = asyncio.new_event_loop()
+        loop.run_until_complete(main())
+        loop.close()
     except KeyboardInterrupt:
-            server.stop(10)
-
-    print("Server stopped.")
-    #clean up
-    for i in range(10):
-        (conn, cur) = connCurList[i]
-        cur.close()
-        conn.close()
-
-
-if __name__ == '__main__':
-    logging.basicConfig()
-    serve()
+        AccountService.close()
