4f00837459da0f5be629c8f812f43305d0f4290a
Moody <mooodyhunter@outlook.com>
2022-03-01 22:18:50 +0000

    move javascript clients to example_clients/

--- example_clients/account_client.js
+++ example_clients/account_client.js
@@ -1,41 +1,15 @@
-/*
- *
- * Copyright 2015 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
-var PROTO_PATH = __dirname + '/common/account.proto';
-
 var google_protobuf_timestamp = require('google-protobuf/google/protobuf/timestamp_pb');
 var grpc = require('@grpc/grpc-js');
 var protoLoader = require('@grpc/proto-loader');
-var packageDefinition = protoLoader.loadSync(
-    PROTO_PATH,
-    {
-        keepCase: true,
-        longs: String,
-        enums: String,
-        defaults: true,
-        oneofs: true
-    });
-var auth_proto = grpc.loadPackageDefinition(packageDefinition).account_package;
+
+const PROTO_PATH = __dirname + '/../common/account.proto';
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, { keepCase: true, longs: String, enums: String, defaults: true, oneofs: true });
+const Proto = grpc.loadPackageDefinition(packageDefinition).account_package;
+const GRPC_SERVER_ADDRESS = 'localhost:50051';
 
 function main() {
-    var target = 'localhost:50051';
 
-    var client = new auth_proto.AccountService(target,
+    var client = new Proto.AccountService(GRPC_SERVER_ADDRESS,
         grpc.credentials.createInsecure());
 
     client.TryLogin({ username: 'test@gmail.com', password: 'test' }, function (err, response) {
@@ -95,7 +69,7 @@ function main() {
 
     client.ListBusinessAreas({}, (err, resp) => console.log("ListBusinessAreas:", err ? "An error has occurred" : resp))
 
-    client.GetNotifications({userid: 1, targetProfileType: "MENTOR"}, 
+    client.GetNotifications({ userid: 1, targetProfileType: "MENTOR" },
         (err, response) => {
             console.log("GetNotifications:", err ? "An error has occurred" : response);
         });
