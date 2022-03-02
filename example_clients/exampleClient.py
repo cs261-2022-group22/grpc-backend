f1bf74bec2b2019eb2a33257e8840fb690ebe73c
Varun Chodanker <57418924+varunchodanker@users.noreply.github.com>
2022-03-02 16:30:53 +0000

    Fixed python client by allowing access to packages in the parent directory.

--- example_clients/exampleClient.py
+++ example_clients/exampleClient.py
@@ -1,13 +1,16 @@
+import sys
+sys.path.append("..")
+
 import asyncio
 from grpclib.client import Channel
-import compiled_protos.account_package as AccountPackage
+from compiled_protos.account_package import AccountServiceStub
 
 async def main():
     channel = Channel(host="127.0.0.1", port=50051)
-    service = AccountPackage.AccountServiceStub(channel)
-    response = await service.try_login(username="test@gmail.com", password="est")
+    service = AccountServiceStub(channel)
+    response = await service.try_login(username="test@gmail.com", password="test")
     print(response)
 
     channel.close()
 
-asyncio.run(main())  # python 3.7 only
+asyncio.run(main())  # python 3.7+
