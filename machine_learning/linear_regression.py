85ee2cec79586a9db14de166fe2d44b74f11d77c
Moody <mooodyhunter@outlook.com>
2022-03-07 16:14:19 +0000

    a bit refactoring, introduced module entry point

--- machine_learning/linear_regression.py
+++ machine_learning/linear_regression.py
@@ -1,69 +1,74 @@
-#learning rate
+# learning rate
 L = 0.001
 
-def mse_m1_pd(m1,m2,k,n,ind,dep):
+
+def mse_m1_pd(m1, m2, k, n, ind, dep):
     sum = 0
     for i in range(n):
-        c = (2*m1*ind[i][0]**2 + 2*m2*ind[i][0]*ind[i][1] + 
-        2*k*ind[i][0] 
-        - 2*ind[i][0]*dep[i] 
-        )
+        c = (2*m1*ind[i][0]**2 + 2*m2*ind[i][0]*ind[i][1] +
+             2*k*ind[i][0]
+             - 2*ind[i][0]*dep[i]
+             )
 
         sum += c
 
     result = sum / n
     return result
 
-def mse_m2_pd(m1,m2,k,n,ind,dep):
+
+def mse_m2_pd(m1, m2, k, n, ind, dep):
     sum = 0
     for i in range(n):
-        c = (2*m1*ind[i][0]*ind[i][1] + 
-        + 2*m2*ind[i][1]**2 + 
-        2*k*ind[i][1]  
-        - 2*ind[i][1]*dep[i]
-        )
+        c = (2*m1*ind[i][0]*ind[i][1] +
+             + 2*m2*ind[i][1]**2 +
+             2*k*ind[i][1]
+             - 2*ind[i][1]*dep[i]
+             )
 
         sum += c
 
     result = sum / n
     return result
 
-def mse_k_pd(m1,m2,k,n,ind,dep):
+
+def mse_k_pd(m1, m2, k, n, ind, dep):
     sum = 0
     for i in range(n):
         c = (
-        2*m1*ind[i][0] + 
-        2*m2*ind[i][1] 
-        + 2*k 
-        - 2*dep[i])
+            2*m1*ind[i][0] +
+            2*m2*ind[i][1]
+            + 2*k
+            - 2*dep[i])
 
         sum += c
 
     result = sum / n
     return result
 
-def linear_regression_2(m1,m2,k,n,ind,dep):
-    new_m1 = m1 - L * mse_m1_pd(m1,m2,k,n,ind,dep)
-    new_m2 = m2 - L * mse_m2_pd(new_m1,m2,k,n,ind,dep)
-    new_k = k - L * mse_k_pd(new_m1,new_m2,k,n,ind,dep)
+
+def linear_regression_2(m1, m2, k, n, ind, dep):
+    new_m1 = m1 - L * mse_m1_pd(m1, m2, k, n, ind, dep)
+    new_m2 = m2 - L * mse_m2_pd(new_m1, m2, k, n, ind, dep)
+    new_k = k - L * mse_k_pd(new_m1, new_m2, k, n, ind, dep)
     return (new_m1, new_m2, new_k)
 
+
 def perform2VariableLinearRegression(n, ind, dep):
-    #number of instances of training data
+    # number of instances of training data
     # e.g. n = 9
-    #values for each of the two independent variables
+    # values for each of the two independent variables
     # e.g. ind = [(0,30),(1,25),(2,20),(3,15),(4,10),(5,5),(5,0),(5,30),(5,25)]
-    #corresponding values for dependent variable
+    # corresponding values for dependent variable
     # e.g. dep = [1.8,2.7,3.4,3.9,4.4,4.9,5,4,4.4]
-    
-    #initial model parameters
+
+    # initial model parameters
     m1 = 0
     m2 = 0
     k = 0
 
-    #refine with 100 epochs
+    # refine with 100 epochs
     for epoch in range(10000):
-        (m1,m2,k) = linear_regression_2(m1,m2,k,n,ind,dep)
+        (m1, m2, k) = linear_regression_2(m1, m2, k, n, ind, dep)
 
-    #output final model
-    return (m1,m2,k)
+    # output final model
+    return (m1, m2, k)
--- machine_learning/update_models.py
+++ machine_learning/update_models.py
@@ -1,87 +1,88 @@
-from __init__ import dataDirPath, allowRootImports
-allowRootImports()
-
-import os
-import psycopg
-import re
 import csv
+import os
+import re
 
+import psycopg
 from utils import GetConnectionString
-from linear_regression import perform2VariableLinearRegression
 
-conn = psycopg.connect(GetConnectionString())
-cur = conn.cursor()
+from . import dataDirPath
+from .linear_regression import perform2VariableLinearRegression
 
-dataFilenameFormat = re.compile("^([0-9]+)-([0-9]+).csv$")
 
-#will consist of tuples where the first element gives the file path, the second 
-# element gives the first business area id and the third element gives the 
-# second business area id.
-dataFileEntries = []
+def UpdateModels():
+    conn = psycopg.connect(GetConnectionString())
+    cur = conn.cursor()
 
-for filename in os.listdir(dataDirPath):
-    if (
-        (dataFilenameMatch := dataFilenameFormat.match(filename)) is not None 
-        and (businessArea1Id:= int(dataFilenameMatch.group(1))) < 
-        (businessArea2Id := int(dataFilenameMatch.group(2)))
-    ):
-        dataFileEntries.append(
-            (
-                os.path.join(dataDirPath, filename), 
-                businessArea1Id, 
-                businessArea2Id
+    dataFilenameFormat = re.compile("^([0-9]+)-([0-9]+).csv$")
+
+    # will consist of tuples where the first element gives the file path, the second
+    # element gives the first business area id and the third element gives the
+    # second business area id.
+    dataFileEntries = []
+
+    for filename in os.listdir(dataDirPath):
+        if (
+            (dataFilenameMatch := dataFilenameFormat.match(filename)) is not None
+            and (businessArea1Id := int(dataFilenameMatch.group(1))) <
+            (businessArea2Id := int(dataFilenameMatch.group(2)))
+        ):
+            dataFileEntries.append(
+                (
+                    os.path.join(dataDirPath, filename),
+                    businessArea1Id,
+                    businessArea2Id
+                )
             )
-        )
 
-for dataFileEntry in dataFileEntries:
-    (dataFilePath, businessArea1Id, businessArea2Id) = dataFileEntry
-    independentVariableValues = []
-    dependentVariableValues = []
+    for dataFileEntry in dataFileEntries:
+        (dataFilePath, businessArea1Id, businessArea2Id) = dataFileEntry
+        independentVariableValues = []
+        dependentVariableValues = []
 
-    try:
-        with open(dataFilePath, "r") as dataFile:
-            dataFileCsvReader = csv.reader(dataFile)
-            next(dataFileCsvReader) #ignore header line
-            for dataRecord in dataFileCsvReader:
-                independentVariableValues.append(
-                    tuple(
-                        [int(e) for e in dataRecord[:2]]
+        try:
+            with open(dataFilePath, "r") as dataFile:
+                dataFileCsvReader = csv.reader(dataFile)
+                next(dataFileCsvReader)  # ignore header line
+                for dataRecord in dataFileCsvReader:
+                    independentVariableValues.append(
+                        tuple(
+                            [int(e) for e in dataRecord[:2]]
+                        )
                     )
-                )
 
-                dependentVariableValues.append(
-                    float(dataRecord[2])
-                )
-    except Exception as inst:
-        print("An exception has occurred while trying to read the machine learning data from " + str(dataFileEntry) + ". This file will be skipped and the details of the exception will follow:")
-        print(inst)
-        continue
+                    dependentVariableValues.append(
+                        float(dataRecord[2])
+                    )
+        except Exception as inst:
+            print("An exception has occurred while trying to read the machine learning data from " + str(dataFileEntry) + ". This file will be skipped and the details of the exception will follow:")
+            print(inst)
+            continue
 
-    recordCount = len(dependentVariableValues)
+        recordCount = len(dependentVariableValues)
 
-    # print(recordCount)
-    # print(independentVariableValues)
-    # print(dependentVariableValues)
+        # print(recordCount)
+        # print(independentVariableValues)
+        # print(dependentVariableValues)
 
-    m1, m2, k = perform2VariableLinearRegression(recordCount, independentVariableValues, dependentVariableValues)
+        m1, m2, k = perform2VariableLinearRegression(recordCount, independentVariableValues, dependentVariableValues)
 
-    print("---")
-    print(businessArea1Id, businessArea2Id)
-    print(m1, m2, k)
-    # print(str(m1*5 + m2*30 + k))
+        print("---")
+        print(businessArea1Id, businessArea2Id)
+        print(m1, m2, k)
+        # print(str(m1*5 + m2*30 + k))
 
-    cur.execute("""SELECT * FROM RatingModel WHERE businessarea1id = %s AND 
-    businessarea2id = %s;""", (businessArea1Id, businessArea2Id))
+        cur.execute("""SELECT * FROM RatingModel WHERE businessarea1id = %s AND 
+        businessarea2id = %s;""", (businessArea1Id, businessArea2Id))
 
-    if cur.fetchone() is None:
-        cur.execute("""INSERT INTO RatingModel VALUES(%s,%s,%s,%s,%s);""", 
-        (businessArea1Id, businessArea2Id, m1, m2, k))
-    else:
-        cur.execute("""UPDATE RatingModel SET skillOverlapCoefficient = %s, ageDifferenceCoefficient = %s, modelOffset = %s WHERE businessarea1id = %s AND 
-        businessarea2id = %s;""", 
-        (m1, m2, k, businessArea1Id, businessArea2Id))
+        if cur.fetchone() is None:
+            cur.execute("""INSERT INTO RatingModel VALUES(%s,%s,%s,%s,%s);""",
+                        (businessArea1Id, businessArea2Id, m1, m2, k))
+        else:
+            cur.execute("""UPDATE RatingModel SET skillOverlapCoefficient = %s, ageDifferenceCoefficient = %s, modelOffset = %s WHERE businessarea1id = %s AND 
+            businessarea2id = %s;""",
+                        (m1, m2, k, businessArea1Id, businessArea2Id))
 
-    conn.commit()
+        conn.commit()
 
-cur.close()
-conn.close()
+    cur.close()
+    conn.close()
